9ea62dd6aacc23c68f31f128dfe68014735bbf92
Ronit <ronittambe116@gmail.com>
2025-05-03 19:30:49 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach((item) => {
+  item.querySelector(".faq-question").addEventListener("click", () => {
+    // Close all items
+    faqItems.forEach((i) => {
+      i.classList.remove("active");
+      i.querySelector(".arrow-icon").src = "./assets/images/arrow-down.svg";
+    });
+
+    // Toggle current
+    item.classList.add("active");
+    item.querySelector(".arrow-icon").src = "./assets/images/arrow-down.svg";
+  });
+});
